106eba4e6ab3546d6d351858e49025560dd20829
al-312 <albinr312@gmail.com>
2025-07-13 15:02:37 +0530

    added EJS views for todo and login, configured view engine, and associated assets

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,30 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import authController from "./controller/auth.contoller"
 import bodyParser from "body-parser";
 import {authMiddleware} from "./middleware/auth.middleware";
 import todoController from "./controller/todo.controller";
+import {IRequest, IResponse} from "./type";
+
 
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(express.json());
+app.set('view engine', 'ejs');
+app.use(express.static("views"))
 
-app.use("/auth", authController)
-app.use("/todo",authMiddleware, todoController)
 
-app.get('/', (req: Request, res: Response) => {
-    res.json({ message: 'Hello, TypeScript Express!' });
-});
+app.use("/api/auth", authController)
+app.use("/api/todo", authMiddleware, todoController)
+
+app.get("/login", (req: IRequest, res: IResponse) => {
+    res.render('login/index');
+
+})
+app.get("/todo", (req: IRequest, res: IResponse) => {
+    res.render('todo/index',);
+
+})
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
